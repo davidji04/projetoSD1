16e486dff4f27af5f39fbc49c49af7fe66396195
David <d.natal@campus.fct.unl.pt>
2025-04-13 18:59:41 +0100

    imagesResource

--- src/fctreddit/impl/server/rest/ImagesResource.java
+++ src/fctreddit/impl/server/rest/ImagesResource.java
@@ -1,4 +1,68 @@
 package fctreddit.impl.server.rest;
 
-public class ImagesResource {
+import java.util.logging.Logger;
+
+import fctreddit.api.User;
+import fctreddit.api.java.Image;
+import fctreddit.api.java.Result;
+import fctreddit.api.java.Users;
+import fctreddit.api.rest.RestImage;
+import fctreddit.impl.server.java.JavaImage;
+import fctreddit.impl.server.java.JavaUsers;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response.Status;
+
+public class ImagesResource implements RestImage {
+
+  private static final Logger Log = Logger.getLogger(ImagesResource.class.getName());
+
+  final Image impl;
+
+  public ImagesResource() {
+    impl = new JavaImage();
+  }
+
+  @Override
+  public String createImage(String userId, byte[] imageContents, String password) {
+    Log.info("createImage : " + userId + "\n");
+
+    Result<String> res = impl.createImage(userId, imageContents, password);
+    if (!res.isOK()) {
+      throw new WebApplicationException(errorCodeToStatus(res.error()));
+    }
+    return res.value();
+  }
+
+  @Override
+  public byte[] getImage(String userId, String imageId) {
+    Log.info("getImage : user = " + userId + "; image = " + imageId + "\n");
+
+    Result<byte[]> res = impl.getImage(userId, imageId);
+    if (!res.isOK()) {
+      throw new WebApplicationException(errorCodeToStatus(res.error()));
+    }
+    return res.value();
+  }
+
+  @Override
+  public void deleteImage(String userId, String imageId, String password) {
+    Log.info("deleteImage : user = " + userId + "; image = " + imageId + "; pwd = " + password + "\n");
+    Result res = impl.deleteImage(userId, imageId, password);
+    if (!res.isOK()) {
+      throw new WebApplicationException(errorCodeToStatus(res.error()));
+    }
+  }
+
+  protected static Status errorCodeToStatus(Result.ErrorCode error) {
+    Status status = switch (error) {
+      case NOT_FOUND -> Status.NOT_FOUND;
+      case CONFLICT -> Status.CONFLICT;
+      case FORBIDDEN -> Status.FORBIDDEN;
+      case NOT_IMPLEMENTED -> Status.NOT_IMPLEMENTED;
+      case BAD_REQUEST -> Status.BAD_REQUEST;
+      default -> Status.INTERNAL_SERVER_ERROR;
+    };
+
+    return status;
+  }
 }
