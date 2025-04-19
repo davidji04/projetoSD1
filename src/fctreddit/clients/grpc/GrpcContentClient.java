bbfdd5a044895e077d2071b8b40e0bdc44242073
David <d.natal@campus.fct.unl.pt>
2025-04-19 19:27:35 +0100

    GrpcContent feito , DataModel feito para content

--- src/fctreddit/clients/grpc/GrpcContentClient.java
+++ src/fctreddit/clients/grpc/GrpcContentClient.java
@@ -8,14 +8,25 @@ import fctreddit.api.Post;
 import fctreddit.api.java.Result;
 import fctreddit.clients.java.ContentClient;
 import fctreddit.impl.grpc.util.DataModelAdaptor;
-import fctreddit.impl.server.grpc.generated_java.UsersGrpc;
-import fctreddit.impl.server.grpc.generated_java.UsersProtoBuf.CreateUserArgs;
-import fctreddit.impl.server.grpc.generated_java.UsersProtoBuf.CreateUserResult;
+import fctreddit.impl.server.grpc.generated_java.ContentGrpc;
+import fctreddit.impl.server.grpc.generated_java.ContentProtoBuf.ChangeVoteArgs;
+import fctreddit.impl.server.grpc.generated_java.ContentProtoBuf.CreatePostArgs;
+import fctreddit.impl.server.grpc.generated_java.ContentProtoBuf.CreatePostResult;
+import fctreddit.impl.server.grpc.generated_java.ContentProtoBuf.DeletePostArgs;
+import fctreddit.impl.server.grpc.generated_java.ContentProtoBuf.GetPostAnswersArgs;
+import fctreddit.impl.server.grpc.generated_java.ContentProtoBuf.GetPostArgs;
+import fctreddit.impl.server.grpc.generated_java.ContentProtoBuf.GetPostsArgs;
+import fctreddit.impl.server.grpc.generated_java.ContentProtoBuf.GetPostsResult;
+import fctreddit.impl.server.grpc.generated_java.ContentProtoBuf.GrpcPost;
+import fctreddit.impl.server.grpc.generated_java.ContentProtoBuf.UpdatePostArgs;
+import fctreddit.impl.server.grpc.generated_java.ContentProtoBuf.VoteCountResult;
 import io.grpc.Channel;
 import io.grpc.LoadBalancerRegistry;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import io.grpc.internal.PickFirstLoadBalancerProvider;
+import fctreddit.api.java.Result.ErrorCode;
 
 public class GrpcContentClient extends ContentClient {
 
@@ -23,94 +34,164 @@ public class GrpcContentClient extends ContentClient {
     LoadBalancerRegistry.getDefaultRegistry().register(new PickFirstLoadBalancerProvider());
   }
 
-  final UsersGrpc.UsersBlockingStub stub;
+  final ContentGrpc.ContentBlockingStub stub;
 
   public GrpcContentClient(URI serverUrl) {
     Channel channel = ManagedChannelBuilder.forAddress(serverUrl.getHost(), serverUrl.getPort()).usePlaintext().build();
-    stub = UsersGrpc.newBlockingStub(channel).withDeadlineAfter(READ_TIMEOUT, TimeUnit.MILLISECONDS);
-
+    stub = ContentGrpc.newBlockingStub(channel).withDeadlineAfter(READ_TIMEOUT, TimeUnit.MILLISECONDS);
   }
 
   @Override
   public Result<String> createPost(Post post, String userPassword) {
-//    try {
-//      CreateContentResult res = stub.createUser(CreateUserArgs.newBuilder()
-//          .setUser(DataModelAdaptor.User_to_GrpcUser(user))
-//          .build());
-//
-//      return Result.ok(res.getUserId());
-//    } catch (StatusRuntimeException sre) {
-//      return Result.error(statusToErrorCode(sre.getStatus()));
-//    }
-//    return Result.error(statusToErrorCode(sre.getStatus()));
-    throw new UnsupportedOperationException("Unimplemented method 'getPosts'");
+    try {
+      CreatePostResult res = stub
+          .createPost(
+              CreatePostArgs.newBuilder().setPost(DataModelAdaptor.Post_toGrpcPost(post)).setPassword(userPassword)
+                  .build());
 
+      return Result.ok(res.getPostId());
+    } catch (StatusRuntimeException sre) {
+      return Result.error(statusToErrorCode(sre.getStatus()));
+    }
   }
 
   @Override
   public Result<List<String>> getPosts(long timestamp, String sortOrder) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getPosts'");
+    try {
+      GetPostsResult res = stub.getPosts(GetPostsArgs.newBuilder()
+          .setTimestamp(timestamp).setSortOrder(sortOrder)
+          .build());
+
+      return Result.ok(res.getPostIdList()); // nao sei se é isto
+    } catch (StatusRuntimeException sre) {
+      return Result.error(statusToErrorCode(sre.getStatus()));
+    }
   }
 
   @Override
   public Result<Post> getPost(String postId) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getPost'");
+    try {
+      GrpcPost res = stub.getPost(GetPostArgs.newBuilder()
+          .setPostId(postId)
+          .build());
+
+      return Result.ok(DataModelAdaptor.GrpcContent_to_Content(res));
+    } catch (StatusRuntimeException sre) {
+      return Result.error(statusToErrorCode(sre.getStatus()));
+    }
   }
 
   @Override
   public Result<List<String>> getPostAnswers(String postId, long maxTimeout) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getPostAnswers'");
+    try {
+      GetPostsResult res = stub
+          .getPostAnswers(GetPostAnswersArgs.newBuilder().setPostId(postId).setTimeout(maxTimeout).build());
+      return Result.ok(res.getPostIdList()); // tambem nao sei se esta bem
+    } catch (StatusRuntimeException sre) {
+      return Result.error(statusToErrorCode(sre.getStatus()));
+    }
   }
 
   @Override
   public Result<Post> updatePost(String postId, String userPassword, Post post) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'updatePost'");
+    try {
+      GrpcPost res = stub
+          .updatePost(UpdatePostArgs.newBuilder().setPostId(postId).setPassword(userPassword)
+              .setPost(DataModelAdaptor.Post_toGrpcPost(post)).build());
+      return Result.ok(DataModelAdaptor.GrpcContent_to_Content(res));
+    } catch (StatusRuntimeException sre) {
+      return Result.error(statusToErrorCode(sre.getStatus()));
+    }
   }
 
   @Override
   public Result<Void> deletePost(String postId, String userPassword) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'deletePost'");
+    try {
+      stub.deletePost(DeletePostArgs.newBuilder().setPostId(postId).setPassword(userPassword).build());
+      return Result.ok();
+    } catch (StatusRuntimeException sre) {
+      return Result.error(statusToErrorCode(sre.getStatus()));
+    }
   }
 
   @Override
   public Result<Void> upVotePost(String postId, String userId, String userPassword) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'upVotePost'");
+    try {
+      stub.upVotePost(
+          ChangeVoteArgs.newBuilder().setPostId(postId).setUserId(userId).setPassword(userPassword).build());
+      return Result.ok();
+    } catch (StatusRuntimeException sre) {
+      return Result.error(statusToErrorCode(sre.getStatus()));
+    }
+
   }
 
   @Override
   public Result<Void> removeUpVotePost(String postId, String userId, String userPassword) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'removeUpVotePost'");
+    try {
+      stub.removeUpVotePost(
+          ChangeVoteArgs.newBuilder().setPostId(postId).setUserId(userId).setPassword(userPassword).build());
+      return Result.ok();
+    } catch (StatusRuntimeException sre) {
+      return Result.error(statusToErrorCode(sre.getStatus()));
+    }
   }
 
   @Override
   public Result<Void> downVotePost(String postId, String userId, String userPassword) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'downVotePost'");
+    try {
+      stub.downVotePost(
+          ChangeVoteArgs.newBuilder().setPostId(postId).setUserId(userId).setPassword(userPassword).build());
+      return Result.ok();
+    } catch (StatusRuntimeException sre) {
+      return Result.error(statusToErrorCode(sre.getStatus()));
+    }
   }
 
   @Override
   public Result<Void> removeDownVotePost(String postId, String userId, String userPassword) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'removeDownVotePost'");
+    try {
+      stub.removeDownVotePost(
+          ChangeVoteArgs.newBuilder().setPostId(postId).setUserId(userId).setPassword(userPassword).build());
+      return Result.ok();
+    } catch (StatusRuntimeException sre) {
+      return Result.error(statusToErrorCode(sre.getStatus()));
+    }
   }
 
   @Override
   public Result<Integer> getupVotes(String postId) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getupVotes'");
+    try {
+      VoteCountResult res = stub
+          .getUpVotes(GetPostArgs.newBuilder().setPostId(postId).build());
+      return Result.ok(res.getCount());
+    } catch (StatusRuntimeException sre) {
+      return Result.error(statusToErrorCode(sre.getStatus()));
+    }
+
   }
 
   @Override
   public Result<Integer> getDownVotes(String postId) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getDownVotes'");
+    try {
+      VoteCountResult res = stub
+          .getDownVotes(GetPostArgs.newBuilder().setPostId(postId).build());
+      return Result.ok(res.getCount());
+    } catch (StatusRuntimeException sre) {
+      return Result.error(statusToErrorCode(sre.getStatus()));
+    }
+  }
+
+  static ErrorCode statusToErrorCode(Status status) {
+    return switch (status.getCode()) {
+      case OK -> ErrorCode.OK;
+      case NOT_FOUND -> ErrorCode.NOT_FOUND;
+      case ALREADY_EXISTS -> ErrorCode.CONFLICT;
+      case PERMISSION_DENIED -> ErrorCode.FORBIDDEN;
+      case INVALID_ARGUMENT -> ErrorCode.BAD_REQUEST;
+      case UNIMPLEMENTED -> ErrorCode.NOT_IMPLEMENTED;
+      default -> ErrorCode.INTERNAL_ERROR;
+    };
   }
 
 }
--- src/fctreddit/impl/grpc/util/DataModelAdaptor.java
+++ src/fctreddit/impl/grpc/util/DataModelAdaptor.java
@@ -1,44 +1,85 @@
 package fctreddit.impl.grpc.util;
 
+import fctreddit.api.Post;
 import fctreddit.api.User;
+import fctreddit.impl.server.grpc.generated_java.ContentProtoBuf.GrpcPost;
 import fctreddit.impl.server.grpc.generated_java.UsersProtoBuf.GrpcUser;
 import fctreddit.impl.server.grpc.generated_java.UsersProtoBuf.GrpcUser.Builder;
 
 public class DataModelAdaptor {
 
-	//Notice that optional values in a Message might not have an
-	//assigned value (although for Strings default value is "") so
-	//before assigning we check if the field has a value, if not
-	//we assign null.
-	public static User GrpcUser_to_User( GrpcUser from )  {
-		return new User( 
-				from.hasUserId() ? from.getUserId() : null, 
+	// Notice that optional values in a Message might not have an
+	// assigned value (although for Strings default value is "") so
+	// before assigning we check if the field has a value, if not
+	// we assign null.
+	public static User GrpcUser_to_User(GrpcUser from) {
+		return new User(
+				from.hasUserId() ? from.getUserId() : null,
 				from.hasFullName() ? from.getFullName() : null,
-				from.hasEmail() ? from.getEmail() : null, 
+				from.hasEmail() ? from.getEmail() : null,
 				from.hasPassword() ? from.getPassword() : null,
 				from.hasAvatarUrl() ? from.getAvatarUrl() : null);
 	}
 
-	//Notice that optional values might not have a value, and 
-	//you should never assign null to a field in a Message
-	public static GrpcUser User_to_GrpcUser( User from )  {
+	public static Post GrpcContent_to_Content(GrpcPost from) {
+		return new Post(
+				from.hasPostId() ? from.getPostId() : null,
+				from.hasAuthorId() ? from.getAuthorId() : null,
+				from.hasCreationTimestamp() ? from.getCreationTimestamp() : null,
+				from.hasContent() ? from.getContent() : null,
+				from.hasMediaUrl() ? from.getMediaUrl() : null,
+				from.hasParentUrl() ? from.getParentUrl() : null,
+				from.hasUpVote() ? from.getUpVote() : 0,
+				from.hasDownVote() ? from.getDownVote() : 0);
+	}
+
+	public static GrpcPost Post_toGrpcPost(Post from) {
+		fctreddit.impl.server.grpc.generated_java.ContentProtoBuf.GrpcPost.Builder b = GrpcPost.newBuilder();
+
+		if (from.getPostId() != null)
+			b.setPostId(from.getPostId());
+
+		if (from.getAuthorId() != null)
+			b.setAuthorId(from.getAuthorId());
+
+		if (from.getCreationTimestamp() != System.currentTimeMillis())
+			b.setCreationTimestamp(from.getCreationTimestamp());
+
+		if (from.getContent() != null)
+			b.setContent(from.getContent());
+
+		if (from.getMediaUrl() != null)
+			b.setMediaUrl(from.getMediaUrl());
+
+		if (from.getParentUrl() != null)
+			b.setParentUrl(from.getParentUrl());
+
+		b.setUpVote(from.getUpVote() != 0 ? from.getUpVote() : 0);
+		b.setDownVote(from.getDownVote() != 0 ? from.getDownVote() : 0);
+
+		return b.build();
+	}
+
+	// Notice that optional values might not have a value, and
+	// you should never assign null to a field in a Message
+	public static GrpcUser User_to_GrpcUser(User from) {
 		Builder b = GrpcUser.newBuilder();
-		
-		if(from.getUserId() != null)
-			b.setUserId( from.getUserId());
-		
-		if(from.getPassword() != null)
-			b.setPassword( from.getPassword());
-		
-		if(from.getEmail() != null)
-			b.setEmail( from.getEmail());
-		
-		if(from.getFullName() != null)
-			b.setFullName( from.getFullName());
-		
-		if(from.getAvatarUrl() != null)
-			b.setAvatarUrl( from.getAvatarUrl());
-		
+
+		if (from.getUserId() != null)
+			b.setUserId(from.getUserId());
+
+		if (from.getPassword() != null)
+			b.setPassword(from.getPassword());
+
+		if (from.getEmail() != null)
+			b.setEmail(from.getEmail());
+
+		if (from.getFullName() != null)
+			b.setFullName(from.getFullName());
+
+		if (from.getAvatarUrl() != null)
+			b.setAvatarUrl(from.getAvatarUrl());
+
 		return b.build();
 	}
 
